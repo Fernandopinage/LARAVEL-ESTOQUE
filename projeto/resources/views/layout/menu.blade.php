8107437a0d7e1a7348d123801ef0b6ec75a2d0fb
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-01-06 15:14:12 -0300

    atualização v01

--- projeto/resources/views/layout/menu.blade.php
+++ projeto/resources/views/layout/menu.blade.php
@@ -8,7 +8,7 @@
     <nav>
 
         <a href=""><div class="link">Home</div></a>
-        <a href=""><div class="link">Home</div></a>
+        <a href=""><div class="link">ADD Funcionario</div></a>
         <a href=""><div class="link">Home</div></a>
         <a href=""><div class="link">Home</div></a>
         <a href=""><div class="link">Home</div></a>
