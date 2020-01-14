e53f78e1c49153e2756e2d525b5829e17db4d3e9
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-01-14 16:58:48 -0300

    msg

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -4,7 +4,7 @@
 /************************************************************************************************ */
 /*                         Sistema desenvolvido para estudo                                       */
 /*                               creiado 14/01/2020                                               */
-/*          proibida a venda desse sistema sem autorização do desenvolvedor                       */
+/*          proibida a venda ou copia desse sistema sem autorização do desenvolvedor              */
 /*                      entre encontado com Luizfernandoluck@hotmail.com                          */
 /*                                                                                                */
 /************************************************************************************************ */
